55c20a7285df1122ee26fe14c700142caaf863a9
Jupiter <110947014+jupiter-softdev@users.noreply.github.com>
2022-09-15 15:40:00 -0400

    Jupiter
    jupiter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from 'react';
-
 import web3 from './connection/web3';
 import Navbar from './components/Layout/Navbar';
 import Main from './components/Content/Main';
@@ -13,23 +12,23 @@ const App = () => {
   const web3Ctx = useContext(Web3Context);
   const collectionCtx = useContext(CollectionContext);
   const marketplaceCtx = useContext(MarketplaceContext);
-  
+
   useEffect(() => {
     // Check if the user has Metamask active
-    if(!web3) {
+    if (!web3) {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
       return;
     }
-    
+
     // Function to fetch all the blockchain data
-    const loadBlockchainData = async() => {
+    const loadBlockchainData = async () => {
       // Request accounts acccess if needed
       try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });  
-      } catch(error) {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+      } catch (error) {
         console.error(error);
       }
-      
+
       // Load account
       const account = await web3Ctx.loadAccount(web3);
 
@@ -43,69 +42,69 @@ const App = () => {
       const mktDeployedNetwork = NFTMarketplace.networks[networkId];
       const mktContract = marketplaceCtx.loadContract(web3, NFTMarketplace, mktDeployedNetwork);
 
-      if(nftContract) {        
+      if (nftContract) {
         // Load total Supply
         const totalSupply = await collectionCtx.loadTotalSupply(nftContract);
-        
+
         // Load Collection
-        collectionCtx.loadCollection(nftContract, totalSupply);       
+        collectionCtx.loadCollection(nftContract, totalSupply);
 
         // Event subscription
         nftContract.events.Transfer()
-        .on('data', (event) => {
-          collectionCtx.updateCollection(nftContract, event.returnValues.tokenId, event.returnValues.to);
-          collectionCtx.setNftIsLoading(false);
-        })
-        .on('error', (error) => {
-          console.log(error);
-        });
-        
+          .on('data', (event) => {
+            collectionCtx.updateCollection(nftContract, event.returnValues.tokenId, event.returnValues.to);
+            collectionCtx.setNftIsLoading(false);
+          })
+          .on('error', (error) => {
+            console.log(error);
+          });
+
       } else {
         window.alert('NFTCollection contract not deployed to detected network.')
       }
 
-      if(mktContract) {
+      if (mktContract) {
         // Load offer count
         const offerCount = await marketplaceCtx.loadOfferCount(mktContract);
-        
+
         // Load offers
-        marketplaceCtx.loadOffers(mktContract, offerCount); 
-        
+        marketplaceCtx.loadOffers(mktContract, offerCount);
+
         // Load User Funds
         account && marketplaceCtx.loadUserFunds(mktContract, account);
 
         // Event OfferFilled subscription 
         mktContract.events.OfferFilled()
-        .on('data', (event) => {
-          marketplaceCtx.updateOffer(event.returnValues.offerId);
-          collectionCtx.updateOwner(event.returnValues.id, event.returnValues.newOwner);
-          marketplaceCtx.setMktIsLoading(false);
-        })
-        .on('error', (error) => {
-          console.log(error);
-        });
+          .on('data', (event) => {
+            marketplaceCtx.updateOffer(event.returnValues.offerId);
+            collectionCtx.updateOwner(event.returnValues.id, event.returnValues.newOwner);
+            marketplaceCtx.setMktIsLoading(false);
+          })
+          .on('error', (error) => {
+            console.log(error);
+          });
 
         // Event Offer subscription 
         mktContract.events.Offer()
-        .on('data', (event) => {
-          marketplaceCtx.addOffer(event.returnValues);
-          marketplaceCtx.setMktIsLoading(false);
-        })
-        .on('error', (error) => {
-          console.log(error);
-        });
+          .on('data', (event) => {
+            marketplaceCtx.addOffer(event.returnValues);
+            marketplaceCtx.setMktIsLoading(false);
+          })
+          .on('error', (error) => {
+            console.log(error);
+          });
 
         // Event offerCancelled subscription 
         mktContract.events.OfferCancelled()
-        .on('data', (event) => {
-          marketplaceCtx.updateOffer(event.returnValues.offerId);
-          collectionCtx.updateOwner(event.returnValues.id, event.returnValues.owner);
-          marketplaceCtx.setMktIsLoading(false);
-        })
-        .on('error', (error) => {
-          console.log(error);
-        });
-        
+          .on('data', (event) => {
+            marketplaceCtx.updateOffer(event.returnValues.offerId);
+            collectionCtx.updateOwner(event.returnValues.id, event.returnValues.owner);
+            marketplaceCtx.setMktIsLoading(false);
+          })
+          .on('error', (error) => {
+            console.log(error);
+          });
+
       } else {
         window.alert('NFTMarketplace contract not deployed to detected network.')
       }
@@ -124,14 +123,13 @@ const App = () => {
         window.location.reload();
       });
     };
-    
+
     loadBlockchainData();
   }, []);
 
   const showNavbar = web3 && collectionCtx.contract && marketplaceCtx.contract;
   const showContent = web3 && collectionCtx.contract && marketplaceCtx.contract && web3Ctx.account;
-  
-  return(
+  return (
     <React.Fragment>
       {showNavbar && <Navbar />}
       {showContent && <Main />}
